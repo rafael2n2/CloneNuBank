0ca0f8a664da62b7963e31ff511d39d9196586c5
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-04-11 16:12:36 -0300

    remove imports

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, TouchableOpacity, Text, ScrollView } from "react-native";
+import { View, TouchableOpacity, Text} from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import styles from "./style";
 
